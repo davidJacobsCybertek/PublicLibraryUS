6a2040e6d8fd15ea770bd50a5abd0b8e1f34618b
Rreze Gjuka <rreze@cybertekschool.com>
2021-05-26 20:46:37 +0200

    commiting

--- src/test/java/com/PubLibrary/pages/UsersPage.java
+++ src/test/java/com/PubLibrary/pages/UsersPage.java
@@ -53,8 +53,13 @@ public class UsersPage extends BasePage{
     @FindBy(css = "[type='search']")
     public WebElement seachBox;
 
+    @FindBy (css = ".portlet-title>span")
+    public WebElement addUser;
 
-//    this code bloc select user group at ADD USER funcionality according user entrance
+
+
+
+    //    this code bloc select user group at ADD USER funcionality according user entrance
     public void selectUserGroupAdd (String selection){
         Select userList = new Select(userGroupId);
         userList.selectByVisibleText(selection);
